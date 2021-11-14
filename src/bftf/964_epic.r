68ecc115ec2112af204076043e9b9938585158f3
mglucini <mglucini@gmail.com>
2021-11-14 16:39:03 -0300

    exp total

--- src/bftf/964_epic.r
+++ src/bftf/964_epic.r
@@ -45,7 +45,7 @@ kexperimento  <- NA   #NA si se corre la primera vez, un valor concreto si es pa
 
 kscript         <- "964_epic"
 
-karch_dataset    <- "./datasets/dataset_epic_v952.csv.gz"
+karch_dataset    <- "./datasets/dataset_epic_v956S.csv.gz"
 
 kapply_mes       <- c(202101)  #El mes donde debo aplicar el modelo
 
